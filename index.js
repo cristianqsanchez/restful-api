c3d1773ed66c6fe000384a188a7c97b9fec9cd6a
cristianqsanchez <cristianquintero316@gmail.com>
2022-10-23 15:56:34 -0500

    create express app

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Go!")
+import express from 'express';
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.text())
+
+app.get('/', (req, res) => {
+  res.send('Connected')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log('Listening on port', process.env.PORT);
+})
