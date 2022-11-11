5e39940d8eac372f7442575ba1bed76b5f34d379
cristianqsanchez <cristianquintero316@gmail.com>
2022-11-11 18:57:20 -0500

    add mongoose dependency and create connection

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import usersRouter from './routes/users.routes.js'
+import { connect } from 'mongoose'
 
 dotenv.config()
 
@@ -15,6 +16,12 @@ app.get('/', (req, res) => {
 
 app.use('/users', usersRouter)
 
-app.listen(process.env.PORT, () => {
-  console.log('Listening on port', process.env.PORT);
-})
+const bootstrp = async () => {
+  connect(process.env.MONGODB_URL)
+
+  app.listen(process.env.PORT, () => {
+    console.log('Listening on port', process.env.PORT);
+  })
+}
+
+bootstrp()
